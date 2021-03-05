3a44c1744923e9843aaa57b6dc8f0bbabead02ba
sinacetin <60692441+sinacetin@users.noreply.github.com>
2021-03-05 13:30:16 +0300

    First Commit

---
+++ Assets/Mesh2Script.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+namespace RectangleTrainer.Mesh2Script
+{
+    public static class Mesh2Script
+    {
+        public static string ConvertToScript(Mesh mesh, string classname)
+        {
+            //start class
+            string csContent = "using UnityEngine;\n" +
+                               "namespace RectangleTrainer.Mesh2Script.ScriptMesh\n" +
+                               "{\n\tpublic static class " + classname + "\n";
+
+            //upto vertices
+            csContent += "\t{\n" +
+                         "\t\tprivate static Mesh mesh;\n" +
+                         "\t\tpublic static Mesh Mesh\n" +
+                         "\t\t{\n" +
+                         "\t\t\tget\n" +
+                         "\t\t\t{\n" +
+                         "\t\t\t\tif (mesh == null)\n" +
+                         "\t\t\t\t\tmesh = Make();\n\n" +
+                         "\t\t\t\treturn mesh;\n" +
+                         "\t\t\t}\n" +
+                         "\t\t}\n\n" +
+                         "\t\tprivate static Mesh Make()\n" +
+                         "\t\t{\n";
+
+            //vertices
+            csContent += "\t\t\tVector3[] vertices =\n" +
+                         "\t\t\t{\n";
+
+            for(int i = 0; i < mesh.vertices.Length; i++)
+            {
+                csContent += $"\t\t\t\tnew Vector3({mesh.vertices[i].x}f, {mesh.vertices[i].y}f, {mesh.vertices[i].z}f)";
+                if (i < mesh.vertices.Length - 1) csContent += ",";
+                csContent += "\n";
+            }
+
+            csContent += "\t\t\t};\n";
+
+
+            //triangles
+            csContent += "\t\t\tint[] triangles =\n" +
+                         "\t\t\t{\n";
+
+            for (int i = 0; i < mesh.triangles.Length; i+=3)
+            {
+                csContent += $"\t\t\t\t{mesh.triangles[i]}, {mesh.triangles[i+1]}, {mesh.triangles[i+2]}";
+                if (i < mesh.triangles.Length - 1) csContent += ",";
+                csContent += "\n";
+            }
+
+            csContent += "\t\t\t};\n\n";
+
+            //normals
+            csContent += "\t\t\tVector3[] normals =\n" +
+                         "\t\t\t{\n";
+
+            for (int i = 0; i < mesh.normals.Length; i++)
+            {
+                csContent += $"\t\t\t\tnew Vector3({mesh.normals[i].x}f, {mesh.normals[i].y}f, {mesh.normals[i].z}f)";
+                if (i < mesh.normals.Length - 1) csContent += ",";
+                csContent += "\n";
+            }
+
+            csContent += "\t\t\t};\n";
+
+            //wrap class up
+            csContent += "\t\t\tMesh mesh = new Mesh();\n" +
+                         "\t\t\tmesh.vertices = vertices;\n" +
+                         "\t\t\tmesh.triangles = triangles;\n" +
+                         "\t\t\tmesh.normals = normals;\n\n" +
+                         "\t\t\treturn mesh;\n" +
+                         "\t\t}\n" +
+                         "\t}\n" +
+                         "}";
+
+            return csContent;
+        }
+    }
+}
---
+++ Assets/Mesh2ScriptWindow.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace RectangleTrainer.Mesh2Script
+{
+    public class Mesh2ScriptWindow : EditorWindow
+    {
+        Mesh inputMesh;
+        string saveFolder = "RT Hardcoded Meshes";
+        string scriptPrefix = "Mesh";
+        string scriptSuffix = "hardcoded";
+        string scriptName;
+
+        // Add menu named "My Window" to the Window menu
+        [MenuItem("Rectangle Trainer/Mesh to Script")]
+        static void Init()
+        {
+            // Get existing open window or if none, make a new one:
+            Mesh2ScriptWindow window = (Mesh2ScriptWindow)GetWindow(typeof(Mesh2ScriptWindow));
+            window.titleContent = new GUIContent("Mesh to Script");
+            window.Show();
+        }
+
+        void OnGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUILayout.LabelField("Input Mesh", EditorStyles.boldLabel);
+            inputMesh = (Mesh)EditorGUILayout.ObjectField(inputMesh, typeof(Mesh), true);
+
+            if(inputMesh)
+            {
+                if (EditorGUI.EndChangeCheck())
+                    scriptName = FormatName(inputMesh.name);
+
+                EditorGUILayout.LabelField("Script Name", EditorStyles.boldLabel);
+                scriptName = EditorGUILayout.TextField(FormatName(scriptName));
+
+                EditorGUILayout.HelpBox(Filename, MessageType.Info);
+            }
+
+            EditorGUILayout.LabelField("Save Folder", EditorStyles.boldLabel);
+            saveFolder = EditorGUILayout.TextField(saveFolder);
+
+            EditorGUILayout.Space();
+            EditorGUI.BeginDisabledGroup(inputMesh == null); 
+            if (GUILayout.Button("Convert To Script"))
+            {
+                if(!AssetDatabase.IsValidFolder($"Assets/{saveFolder}"))
+                {
+                    AssetDatabase.CreateFolder("Assets", saveFolder);
+                }
+
+                WriteString();
+
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+
+        private string SizeEstimation()
+        {
+//            inputMesh.vertexCount;
+            return "";
+        }
+
+        void WriteString()
+        {
+            string path = "Assets/" + Filename;
+
+            StreamWriter writer = new StreamWriter(path, true);
+            writer.WriteLine(Mesh2Script.ConvertToScript(inputMesh, Classname));
+            writer.Close();
+            AssetDatabase.Refresh();
+        }
+
+        string Filename
+        {
+            get => $"{saveFolder}/{Classname}.cs";
+        }
+        string Classname
+        {
+            get => $"{scriptPrefix}_{scriptName}_{scriptSuffix}";
+        }
+
+        string FormatName(string unformatted)
+        {
+            string formatted = "";
+
+            for(int i = 0; i < unformatted.Length; i++)
+            {
+                char c = unformatted[i];
+                if ((c >= 'a' && c <= 'z') ||
+                    (c >= 'A' && c <= 'Z'))
+                {
+                    formatted += c;
+                }
+                else if(c == ' ' || c == '-' || c == '_' || c == '.')
+                {
+                    formatted += '_';
+                }
+            }
+
+            return formatted;
+        }
+    }
+}
